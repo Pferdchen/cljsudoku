d6626308078cd7985ccd35823b5dd303b0ee16a3
Yan Gao <gaoyan126@googlemail.com>
2022-08-12 15:15:04 +0200

    Print out puzzle1.txt

--- src/cljsudoku/core.clj
+++ src/cljsudoku/core.clj
@@ -4,3 +4,8 @@
   "I don't do a whole lot."
   [x]
   (println x "Hello, World!"))
+
+(require '[clojure.java.io :as jio])
+(with-open [rdr (jio/reader "resources/puzzles/puzzles1.txt")]
+  (doseq [line (line-seq rdr)]
+    (println line)))

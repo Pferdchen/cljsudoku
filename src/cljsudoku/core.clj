83875ed10746c0e7ec81e154c0040c42e7e2f337
Yan Gao <gaoyan126@googlemail.com>
2022-08-21 14:35:21 +0200

    Fix bugs

--- src/cljsudoku/core.clj
+++ src/cljsudoku/core.clj
@@ -38,6 +38,8 @@
    (with-open [rdr (jio/reader path)]
      (mapv (fn [line] (str-to-vec line)) (line-seq rdr)))))
 
+(def full-set (set (range 1 9)))
+
 (defn cell-item
   "Returns the item (number or nil) in the cell."
   [p i j]
@@ -54,10 +56,16 @@
              :when (integer? ci)]
          ci)))
 
+(defn start-end
+  [index]
+  (let [start (* (quot index 3) 3)
+        end (+ start 3)]
+    [start end]))
+
 (defn region-set
   [p i j]
-  (set (for [m (range (quot i 3) (+ (quot i 3) 3))
-             n (range (quot j 3) (+ (quot j 3) 3))
+  (set (for [m (apply range (start-end i))
+             n (apply range (start-end j))
              :let [ci (cell-item p m n)]
              :when (integer? ci)]
          ci)))
@@ -65,9 +73,10 @@
 (defn nil-set
   "Returns a set, that contains all possible numbers for the empty cell."
   [p i j]
-  (into (into (vertical-axis-set p i)
-              (horizontal-axis-set p j))
-        (region-set p i j)))
+  (apply disj (apply disj (apply disj full-set
+                                 (vertical-axis-set p i))
+                     (horizontal-axis-set p j))
+         (region-set p i j)))
 
 (defn cell-set
   [p i j]
@@ -79,3 +88,25 @@
   "Solves a puzzle"
   [p]
   (println p))
+
+(defn puzzle-to-vec
+  "Formatts a puzzle file to one vector."
+  ([] (puzzle-to-vec "resources/puzzles/puzzle1.txt"))
+  ([path]
+   (with-open [rdr (jio/reader path)]
+     (str-to-vec (apply str (line-seq rdr))))))
+
+(defn find-index
+  "Returns an index of vector from a puzzle position."
+  [i j]
+  (+ (* i 9) j))
+
+(defn find-item
+  "Finds an item from puzzle vector."
+  [p-vec i j]
+  (nth p-vec (find-index i j)))
+
+(defn possibilities
+  "Returns a set of possible number."
+  [i j]
+  ())
